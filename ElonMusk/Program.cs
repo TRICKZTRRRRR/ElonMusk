e4ed158043f43c702e1954268871d9d1bf35b0d0
Jonas Steinmejer <jonas04@outlook.dk>
2023-09-11 15:09:47 +0200

    Edited Program.cs

--- ElonMusk/Program.cs
+++ ElonMusk/Program.cs
@@ -10,6 +10,7 @@ namespace ElonMusk
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
---
+++ ElonMusk/RCCar.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElonMusk
+{
+    #region Model
+    public class RCCar
+    {
+        private sbyte batteryCapacity;
+        private int hasDroven;
+        private sbyte batteryLife;
+        private string color;
+
+        public sbyte BatteryCapacity
+        {
+            get { return batteryCapacity; }
+            set { batteryCapacity = value; }
+        }
+
+        public int HasDroven
+        {
+            get { return hasDroven; }
+            private set { hasDroven = value;}
+        }
+
+        public sbyte BatteryLife
+        {
+            get { return batteryLife; }
+            private set { batteryLife = value;}
+        }
+
+        public string Color
+        {
+            get { return color; }
+            private set { color = value; }
+        }
+    }
+    #endregion
+}
